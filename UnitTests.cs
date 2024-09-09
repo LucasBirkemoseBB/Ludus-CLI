5164e01dbaea7d0e770fc4503482bdd260f50e86
LucasBirkemoseBB <lucasallgd@gmail.com>
2024-09-09 13:34:50 +0100

    Fixed unit test errors and working on cli :)

--- UnitTests.cs
+++ UnitTests.cs
@@ -4,7 +4,7 @@ namespace UnitTests
 {
 	public class UnitTests<T>
 	{
-		public delegate bool CompareFunc(T...a);
+		public delegate bool CompareFunc(params T[] param);
 		public static bool Test(T input, T expected, CompareFunc compare) 
 		{
 			return compare(input, expected);
@@ -16,7 +16,7 @@ namespace UnitTests
 	{
 		public void doTest()
 		{
-			UnitTests<int>.Test(Add(3,4), 7, (a, b) => { return a==b; });
+			UnitTests<int>.Test(Add(3,4), 7, (a) => { return a[0]==a[1]; });
 		}
 
 		public int Add(int a, int b)
---
+++ cli.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace cli
+{
+	public class CLI 
+	{
+		
+	}
+}
