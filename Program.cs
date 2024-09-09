b9f1d896367d579a16c6df291acf39e997192a48
LucasBirkemoseBB <lucasallgd@gmail.com>
2024-09-09 10:31:15 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace Program
+{
+	public class Student 
+	{
+		public string navn{get}
+		public int alder{get}
+		public string studieretning{get;set}
+		public bool studieaktiv{get;set}
+
+		override public string ToString() 
+		{
+			return $"{navn} er {alder} år gammel og studere {studieretning}";
+		}
+	}
+
+	internal class Program 
+	{
+		static void main(string[] args)
+		{
+
+		}
+
+
+	}
+}
