7dd9472f863347f180532ed45b5bc98f83fb8a62
LucasBirkemoseBB <lucasallgd@gmail.com>
2024-09-10 09:47:35 +0100

    skame stuff

---
+++ Skema.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SkemaSystem
+{
+  public readonly struct Klasse 
+  {
+    private readonly int teacherId{get;};
+    private readonly int location{get;}
+    private readonly int length{get;}
+    private readonly string room{get;}
+    private readonly string klasse{get;}
+    private readonly string label{get;}
+
+    public Klasse(int teacherId, int location, int length, string room, string klasse, string label)
+    {
+      this.teacherId = teacherId;
+      this.location = location;
+      this.length = length;
+      this.room = room;
+      this.klasse = klasse;
+      this.label = label;
+    }
+  };
+
+  public class Skema 
+  {
+    public List<Klasse> klasser = new List<Klasse>();
+
+    public void addKlasse(Klasse klasse)
+    {
+      klasser.Add(klasse);
+    }
+
+    public void removeKlasse(int identifier)
+    {
+      klasser.RemoveAt(identifier);
+    }
+
+    public void saveToFile(string path)
+    {
+      string json = JsonSerializer.Serialize(klasser);
+      File.WriteAllText(path, json);
+    }
+
+    public void loadFromFile(string path)
+    {
+      string json = File.ReadAllText(path);
+      klasser = JsonSerializer.Deserialize<List<Klasse>>(json);
+    }
+  }
+}
--- obj/Debug/net8.0/Opg1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Opg1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc639591d2b0111d67327f189e791f2d75480630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c58b1654279822f7f8937c7eedfe722f3d2e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
