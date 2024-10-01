33dc48b0fb1d0600e195968a4b476a90cca26877
Samlikekristaps <kristaps.aizpurvietis@gmail.com>
2024-10-01 09:22:40 +0200

    Alle i klassen er blevet registret i KGB database

--- obj/Debug/net8.0/Opg1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Opg1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69aa5e2aa8e714166d0f52cd466eba1a2801358b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da42bf434177ebf15466b24d9d26ae01152c10e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Opg1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
