ef4163747d55b84e5384b13b411be0c38a439b1a
LucasBirkemoseBB <lucasallgd@gmail.com>
2024-10-02 13:18:49 +0100

    Stuff

--- Constants.cs
+++ Constants.cs
@@ -8,7 +8,7 @@ namespace Constants
   public class Consts 
   {
     public static StudentHandler studentHandler = new StudentHandler();
-    public static MessageHandler messageHandler = new MessageHandler("messages.json");
+    public static MessageHandler messageHandler = new MessageHandler();
     public static LektierWorker lektierHandler = new LektierWorker();
   }
 }
--- meddelelser.cs
+++ meddelelser.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace MsgSystem
 {
-  public struct Message 
+  public struct Message
   {
-    public int sender{get;set;}
-    public int recipient{get;set;}
-    public string content{get;set;}
+    public int sender { get; set; }
+    public int recipient { get; set; }
+    public string content { get; set; }
 
-    public Message(int sender, int recipient, string content) 
+    public Message(int sender, int recipient, string content)
     {
       this.sender = sender;
       this.recipient = recipient;
@@ -22,32 +25,48 @@ namespace MsgSystem
 
   };
 
-  public class MessageHandler 
+  public class MessageHandler
   {
     private List<Message> messages = new List<Message>();
-    private string msgs_path;
+    private HttpClient messageClient = new HttpClient();
 
-    public MessageHandler(string path) 
+    public MessageHandler()
     {
-      msgs_path = path;
-
-      if(!File.Exists(path)) return;
-      string json = File.ReadAllText(path);
-      messages = JsonSerializer.Deserialize<List<Message>>(json);
+      // if (!File.Exists(path)) return;
+      // string json = File.ReadAllText(path);
+      // messages = JsonSerializer.Deserialize<List<Message>>(json);
+      loadFromServer();
     }
 
-    public void sendMessage(int sender, int recipient, string content) 
+    public void sendMessage(int sender, int recipient, string content)
     {
-      messages.Add(new Message(sender, recipient, content));
-
-      string json = JsonSerializer.Serialize(messages);
-      File.WriteAllText(msgs_path, json);
+      var message = new Message(sender, recipient, content);
+      messages.Add(message);
+      sendToServer(message);
+      // string json = JsonSerializer.Serialize(messages);
+      // File.WriteAllText(msgs_path, json);
     }
 
     public List<Message> getRecievedMessages(int recipient)
     {
-      if(messages.Count() == 0) return null;
+      if (messages.Count() == 0) return null;
       return messages.FindAll((x) => x.recipient == recipient);
+    }
+
+    public async void loadFromServer()
+    {
+      using HttpResponseMessage response = await messageClient.GetAsync("http://10.144.100.201:4201/");
+      response.EnsureSuccessStatusCode();
+      var responseBody = await response.Content.ReadAsStreamAsync();
+
+      messages = JsonSerializer.Deserialize<List<Message>>(responseBody);
+    }
+
+    public async void sendToServer(Message message)
+    {
+      var content = JsonContent.Create(message);
+      var post = await messageClient.PostAsync("http://10.144.100.201:4201/", content);
+      
     }
   }
 }
